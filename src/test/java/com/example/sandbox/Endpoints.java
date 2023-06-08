b73d28537ec0cb64b7169d2e871f847766f5079d
BaksaZoo <baksazoo@hotmail.com>
2023-06-08 16:20:13 +0200

    implement pet endpoint tests

--- src/test/java/com/example/sandbox/Endpoints.java
+++ src/test/java/com/example/sandbox/Endpoints.java
@@ -6,6 +6,7 @@ public class Endpoints {
 
     //-------------------------pet-------------------------
     public static final String findByStatus = "/pet/findByStatus";
+    public static final String findByTags = "/pet/findByTags";
     public static final String uploadImage = "/pet/{petId}/uploadImage";
     public static final String newPet = "/pet";
     public static final String updatePet = "/pet";
